efdb91ecf47adbc6be232c8ba98ed4f74e45a472
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-02-06 12:22:42 +0000

    scrolling up date

--- scriptNew.js
+++ scriptNew.js
@@ -4,7 +4,7 @@ fetch("https://dummyjson.com/users")
     // data.user.slice
     console.log()
 
-    const nerArr = data.users.slice(0,10);
+    const nerArr = data.users.slice(0,15);
     const carousel = document.querySelector(".carousel");
     // console.log(carousel);
 
@@ -51,7 +51,7 @@ fetch("https://dummyjson.com/users")
             // if(carousel.scrollLeft === )
                 
         
-            console.log(carousel.scrollLeft + carousel.offsetWidth + 5 , ":", carousel.scrollWidth )
+            // console.log(carousel.scrollLeft + carousel.offsetWidth + 5 , ":", carousel.scrollWidth )
             // console.log(carousel.offsetWidth, "offsetWidth Display")
             // console.log(carousel.scrollWidth, "scrollWidth total Width")
             // console.log(carousel.scrollLeft, "scrollLeft")
@@ -64,14 +64,59 @@ fetch("https://dummyjson.com/users")
         })
     const wrapper = document.querySelector(".carousel");
         // console.log(carousel.scrollWidth === carousel.scrollLeft)
-        
-
-
-
-        
+                
     })
 
 
+// const carousel = document.querySelector(".carousel");
 
+
+let isDragging = false, startX, startScrollLeft;
+// let startX ;
+// let startScrollLeft;
+
+const dragStart = (e)=>{
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Record the initial cursor and scroll position of the carousel.
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+    
+    console.log(startX, "startX")
+    const leftArrow = document.querySelector("#left")
+    const rightArrow = document.querySelector("#right")
+    if(carousel.scrollLeft == 0){
+        leftArrow.style.display = "none";
+        // leftArrow.style.backgroundColor = "red";
+    }else{
+        leftArrow.style.display = "block";
+        // leftArrow.style.backgroundColor = "green";
+
+    }
+    if(carousel.offsetWidth + carousel.scrollLeft + 5  > carousel.scrollWidth){
+        rightArrow.style.display = "none";
+    }else{
+        rightArrow.style.display = "block";
+
+    }
+    
+}
+
+const dragging = (e)=>{
+    if(!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement 
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+
+
+}
+
+const dragStop =()=>{
+    isDragging = false;
+    carousel.classList.remove("dragging")
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
 
 })
